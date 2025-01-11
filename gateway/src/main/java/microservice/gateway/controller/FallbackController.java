1bb56276a7737dbfd3cf5e9bb445ee464e6392f0
phamduykhanh98 <134706244+phamduykhanh98@users.noreply.github.com>
2025-01-11 10:32:45 +0700

    circuit breaker

--- gateway/src/main/java/microservice/gateway/controller/FallbackController.java
+++ gateway/src/main/java/microservice/gateway/controller/FallbackController.java
@@ -4,18 +4,20 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ServerWebExchange;
 
 @RestController
-@RequestMapping("/fallbackRoute")
+@RequestMapping("/fallback")
 @Slf4j
 public class FallbackController {
 
-    @GetMapping
-    public ResponseEntity<String> fallback() {
-        return ResponseEntity.status(HttpStatus.SERVICE_UNAVAILABLE)
-                .body("Service is temporarily unavailable. Please try again later.");
+    @GetMapping("/{service}")
+    public ResponseEntity<String> fallback(@PathVariable String service) {
+
+        return ResponseEntity
+                .status(HttpStatus.SERVICE_UNAVAILABLE)
+                .body(service + " service is not working");
     }
 }
